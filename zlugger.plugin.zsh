d4fd6fa8652debc6575149c4ee37ce5c75196676
Marcus R. Brown <contact@marcusrbrown.com>
2021-02-25 02:54:58 -0700

    feat: add configuration and a couple of functions

---
+++ zlugger.plugin.zsh
@@ -0,0 +1,10 @@
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+if [[ $PMSPEC != *f* ]] {
+  fpath+=( "${0:h}/functions" )
+}
+
+autoload -Uz \
+  +zlug-write-script-header \
+  @zlug-from-deferred-source
